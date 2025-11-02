2dbe95c324e33c513a52620ebdc14cf9dd7be9d5
bharath <79780989+doctorbhh@users.noreply.github.com>
2025-11-02 22:51:30 +0530

    Update wipe.tsx

--- app/routes/wipe.tsx
+++ app/routes/wipe.tsx
@@ -15,12 +15,12 @@ const WipeApp = () => {
   if (!isMounted) {
     // optionally show a loading or skeleton while waiting
     return (
-      <div className="p-10>
+      <main>
         <Navbar />
         <div className="flex items-center justify-center text-gray-500 mt-10">
           Loading...
         </div>
-      </div>
+      </main>
     );
   }
 
