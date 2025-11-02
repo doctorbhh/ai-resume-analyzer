b5cf7af7885f628fb446242fa2fdbcb8be9a3844
bharath <79780989+doctorbhh@users.noreply.github.com>
2025-11-02 19:21:16 +0530

    Update AboutUs.tsx

--- app/routes/AboutUs.tsx
+++ app/routes/AboutUs.tsx
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import Card from "~/components/AboutCard";
 import Navbar from "~/components/Navbar";
 import { Icon1, Icon2, Icon3, Icon4 } from "~/components/AboutCard";
-
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resanalyzer" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
 const AboutUs = () => {
   const [isClient, setIsClient] = useState(false);
 
