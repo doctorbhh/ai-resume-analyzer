28cd649b457f71b1f2cd6aaafd2602415f031597
bharath <79780989+doctorbhh@users.noreply.github.com>
2025-11-02 22:45:59 +0530

    Update AboutUs.tsx

--- app/routes/AboutUs.tsx
+++ app/routes/AboutUs.tsx
@@ -2,12 +2,7 @@ import React, { useEffect, useState } from "react";
 import Card from "~/components/AboutCard";
 import Navbar from "~/components/Navbar";
 import { Icon1, Icon2, Icon3, Icon4 } from "~/components/AboutCard";
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Resanalyzer" },
-    { name: "description", content: "Smart feedback for your dream job!" },
-  ];
-}
+
 const AboutUs = () => {
   const [isClient, setIsClient] = useState(false);
 
@@ -19,26 +14,14 @@ const AboutUs = () => {
   if (!isClient) return null;
 
   return (
-    <div className="p-10 min-h-screen">
+    <main>
       <Navbar />
-
-      <div className="flex justify-center">
-        {/* Left icons */}
-        <div className="flex  gap-5 items-center mr-10">
-          <Icon1 />
-          <Icon2 />
-        </div>
-
-        {/* Center Card */}
-        <Card />
-
-        {/* Right icons */}
-        <div className="flex  gap-5 items-center ml-10">
-          <Icon3 />
-          <Icon4 />
+      <div>
+        <div className="flex justify-center">
+          <Card />
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
